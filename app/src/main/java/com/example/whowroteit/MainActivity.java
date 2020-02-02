1c1e2f43d994eff78526db4775846a810779f1b8
Ryan Freeman <info@ryanfreeman.io>
2020-02-02 23:29:07 +0000

    Added implementation of AsyncLoader in MainActivity

--- app/src/main/java/com/example/whowroteit/MainActivity.java
+++ app/src/main/java/com/example/whowroteit/MainActivity.java
@@ -15,6 +15,9 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
     private EditText mBookInput;
     private TextView mTitleText;
@@ -28,6 +31,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mBookInput = (EditText) findViewById(R.id.bookInput);
         mTitleText = (TextView) findViewById(R.id.titleText);
         mAuthorText = (TextView) findViewById(R.id.authorText);
+
+        if (getSupportLoaderManager().getLoader(0) != null) {
+            // reconnect to the loader if it exists, preserves the data in case of configuration change
+            getSupportLoaderManager().initLoader(0, null, this);
+        }
     }
 
     public void searchBooks(View view) {
@@ -71,12 +79,67 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @NonNull
     @Override
     public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
-        return null;
+        String queryString = "";
+
+        if (args != null) {
+            queryString = args.getString("queryString");
+        }
+
+        return new BookLoader(this, queryString);
     }
 
     @Override
     public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            // Convert the response into a JSON object.
+            JSONObject jsonObject = new JSONObject(data);
+            // Get the JSONArray of book items.
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
 
+            // Initialize iterator and results fields.
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            // Look for results in the items array, exiting
+            // when both the title and author
+            // are found or when all items have been checked.
+            while (i < itemsArray.length() &&
+                    (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                i++;
+            }
+
+            // If both are found, display the result.
+            if (title != null && authors != null) {
+                mTitleText.setText(title);
+                mAuthorText.setText(authors);
+            } else {
+                // If none are found, update the UI to
+                // show failed results.
+                mTitleText.setText(R.string.no_results);
+                mAuthorText.setText("");
+            }
+
+        } catch (Exception e) {
+            // If onPostExecute does not receive a proper JSON string,
+            // update the UI to show failed results.
+            mTitleText.setText(R.string.no_results);
+            mAuthorText.setText("");
+        }
     }
 
     @Override
